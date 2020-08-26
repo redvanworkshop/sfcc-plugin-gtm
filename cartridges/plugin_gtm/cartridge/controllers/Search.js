185314ee7ba670e4059fef3bcd8160945060123f
Aldo Secaida <asecaida@gmail.com>
2020-08-26 10:37:07 -0700

    Removed route:BeforeComplete as the latest SFRA version no longer has it.  Also removed htmlHead.isml as the latest SFRA version is updated to this file.

--- cartridges/plugin_gtm/cartridge/controllers/Search.js
+++ cartridges/plugin_gtm/cartridge/controllers/Search.js
@@ -7,13 +7,11 @@ var gtmHelpers = require('*/cartridge/scripts/gtm/gtmHelpers');
 
 server.append('UpdateGrid', function (req, res, next) {
     if (gtmHelpers.isEnabled) {
-        this.on('route:BeforeComplete', function (req, res) { // eslint-disable-line no-shadow
-            var viewData = res.getViewData();
-            var searchImpressionData = gtmHelpers.getSearchImpressionData(viewData);
-            searchImpressionData.event = 'searchImpressions';
-            viewData.searchImpressionData = JSON.stringify(searchImpressionData);
-            res.setViewData(viewData);
-        });
+		var viewData = res.getViewData();
+		var searchImpressionData = gtmHelpers.getSearchImpressionData(viewData);
+		searchImpressionData.event = 'searchImpressions';
+		viewData.searchImpressionData = JSON.stringify(searchImpressionData);
+		res.setViewData(viewData);
     }
     next();
 });
